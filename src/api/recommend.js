4a888a99dbdf1b1445138c990e73a70a6d4fd512
877927262 <877927262@qq.com>
2018-04-04 08:44:48 +0800

    直接用jsonp拿数据失败了接下来自己玩玩

--- src/api/recommend.js
+++ src/api/recommend.js
@@ -1,6 +1,7 @@
 import jsonp from 'common/js/jsonp'
 import {commonParams, options} from './config'
 
+// 获取推荐数据，也就是轮播图的数据
 export function getRecommend () {
   const url = 'https://c.y.qq.com/musichall/fcgi-bin/fcg_yqqhomepagerecommend.fcg'
 
@@ -8,6 +9,23 @@ export function getRecommend () {
     platform: 'h5',
     uin: 0,
     needNewCode: 1
+  })
+
+  return jsonp(url, data, options)
+}
+
+// 获取歌单数据
+export function getDiscList () {
+  const url = 'https://c.y.qq.com/splcloud/fcgi-bin/fcg_get_diss_by_tag.fcg'
+
+  const data = Object.assign({}, commonParams, {
+    rnd: Math.random(),
+    platform: 'yqq',
+    sin: 0,
+    ein: 29,
+    sortId: 5,
+    needNewCode: 0,
+    categoryId: 10000000
   })
 
   return jsonp(url, data, options)
