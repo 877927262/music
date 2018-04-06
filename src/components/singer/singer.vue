895ad6723ce1389a48333d10ec7324fbaec634b1
877927262 <877927262@qq.com>
2018-04-06 17:41:32 +0800

    修改数据结构了，接下来再进一步修改

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,6 +7,10 @@
 <script>
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+
+  let HOT_NAME = '热门'
+  let HOT_SINGER_LEN = 10
 
   export default {
     data() {
@@ -22,9 +26,37 @@
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
             this.singers = res.data.list
-            console.log(this.singers)
+            console.log(this._normalizeSinger(this.singers))
           }
         })
+      },
+      _normalizeSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGER_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        console.log(map)
       }
     }
   }
